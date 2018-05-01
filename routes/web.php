0b8a484143abf09a4ffa66039838e771ff1da77e
kslm <kslm.g00gle@gmail.com>
2018-05-01 16:42:14 +0630

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+
+Auth::routes();
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles','RoleController');
+    Route::resource('users','UserController');
+    Route::resource('products','ProductController');
+});
